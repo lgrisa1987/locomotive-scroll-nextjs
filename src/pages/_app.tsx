ae52b5e23bbfcd97b8618faef41b764ea4c10d45
Leandro <leagrisalena@gmail.com>
2023-01-21 00:23:39 -0300

    locomotive-scroll

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { SiteLayout } from '@/Layout';
+import type { AppProps } from 'next/app';
+import '@/styles/globals.css';
+import 'locomotive-scroll/dist/locomotive-scroll.css';
+import { AnimatePresence } from 'framer-motion';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, router }: AppProps) {
+  return (
+    <SiteLayout>
+      <AnimatePresence mode='wait' initial={false}>
+        <Component {...pageProps} key={router.asPath} />
+      </AnimatePresence>
+    </SiteLayout>
+  );
 }
